072d461a7bd588b268a2ec36c442e654e25a0793
sabine-xuaitsep <sabinepestiaux@gmail.com>
2021-06-11 16:13:33 +0200

    NavMobile designed & implemented
    navmobile_trigger JS component

---
+++ src/hectordenize/js/components/_nav_trigger.js
@@ -0,0 +1,26 @@
+// ./hectordenize/js/components/_nav_trigger.js
+// NAV TRIGGER COMPONENT
+
+"use strict";
+
+export function navTrigger() {
+
+  const menuBtn = document.getElementById('navTrigger');
+
+  menuBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById(this.dataset.target).classList.toggle('navMobileOn');
+    if (this.classList.contains('closeMenu')) {
+      menuBtn.classList.remove('closeMenu');
+    }
+    else {
+      setTimeout(function(){ menuBtn.classList.add('closeMenu'); }, 700);
+    }
+  });
+
+  menuBtn.addEventListener('blur', function (e) {
+    e.preventDefault();
+    document.getElementById(this.dataset.target).classList.remove('navMobileOn');
+    this.classList.remove('closeMenu');
+  });
+}
--- src/hectordenize/js/index.js
+++ src/hectordenize/js/index.js
@@ -1,2 +1,4 @@
 // ./hectordenizeCSS/js/index.js
 // JS FRAMEWORK DISPATCHER
+
+import {navTrigger} from './components/_nav_trigger'; navTrigger();
